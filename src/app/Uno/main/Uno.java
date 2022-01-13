6dfd0825c5c94d8f1b5d0cff94a8ce82a5478337
Daniel Jaimes <48568101+daniel-jaimes@users.noreply.github.com>
2022-01-13 19:31:45 +0100

    Create README.md and main, controller class

---
+++ src/app/Uno/main/Uno.java
@@ -0,0 +1,9 @@
+package main;
+
+import manager.Controller;
+
+public class Uno {
+    public static void main(String[] args) {
+        Controller.init();
+    }
+}
---
+++ src/app/Uno/manager/Controller.java
@@ -0,0 +1,7 @@
+package manager;
+
+public class Controller {
+
+    public synchronized static void init() {
+    }
+}
