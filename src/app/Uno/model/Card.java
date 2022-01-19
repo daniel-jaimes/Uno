c5abd857c19e87afb51045c4ca8bd4cac6ca3e75
daniel--jaimes <48568101+daniel-jaimes@users.noreply.github.com>
2022-01-19 20:10:53 +0100

    add mysql dependency create CardIdentifier and Card constructor

--- src/app/Uno/model/Card.java
+++ src/app/Uno/model/Card.java
@@ -5,4 +5,8 @@ import utils.CardIdentifier;
 public class Card {
     private int id;
     private CardIdentifier value;
+    public Card(int id, CardIdentifier value){
+        this.id = id;
+        this.value = value;
+    }
 }
--- src/app/Uno/utils/CardIdentifier.java
+++ src/app/Uno/utils/CardIdentifier.java
@@ -1,10 +1,16 @@
 package utils;
 
 public class CardIdentifier {
+    private Color color;
+    private Number number;
+    public CardIdentifier(Color color, Number number){
+        this.color = color;
+        this.number = number;
+    }
     public enum Color {
         ROJO, AMARILLO, VERDE, AZUL, NEGRO;
     }
-    public enum Numero {
+    public enum Number {
         CERO, UNO, DOS, TRES, CUATRO, CINCO, SEIS, SIETE, OCHO,
         NUEVE, CAMBIO, MASDOS, SALTO, CAMBIOCOLOR, MAS_CUATRO;
     }
